e523230175a628175c8edb33c6b7098c8f438cea
Tus-GitHub <tushardhull22@gmail.com>
2024-09-21 18:31:07 +0530

    created components and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
-export default function App() {
-  return (
-    <div>App</div>
-  )
-};
+export default function App(){
+ return (
+  <BrowserRouter >
+    <Routes>
+      <Route path = "/" element={<Home />}/>
+      <Route path = '/sign-up' element={<SignUp/>} />
+      <Route path = '/sign-in' element={<SignIn/>} />
+    </Routes>
+  </BrowserRouter>
+ )
+}
