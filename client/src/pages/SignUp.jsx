14c8b33f3bf5128d01fbe02eadb4f96bccca3bc4
Tus-GitHub <tushardhull22@gmail.com>
2024-09-23 18:51:47 +0530

    create signup page Ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
+  const[formData, setFormData] = useState({});
+
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+  }
+
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form 
+        className = 'flex flex-col gap-4'
+        onSubmit={handleSubmit}
+      >
+        <input 
+          type = 'text'
+          placeholder = 'Username'
+          className = 'border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input 
+          type = 'text'
+          placeholder = 'Email'
+          className = 'border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input 
+          type = 'text'
+          placeholder = 'Password'
+          className = 'border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+        <button
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          Sign Up
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an Account?</p>
+        <Link to={'/sign-in'}>
+        <span className='text-blue-700'>Sign In</span>
+        </Link>
+      </div>
+    </div>
   )
-}
+};
